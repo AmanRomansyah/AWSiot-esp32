35b3b491e5e577f5779587050c7e4da1e3a91656
AmanRomansyah <romansyah7714@gmail.com>
2020-04-17 20:20:52 +0700

    menambahkan program AWS dan WIFI

---
+++ AWS.ino
@@ -0,0 +1,52 @@
+void connectToAWS()
+{
+  // Configure WiFiClientSecure to use the AWS certificates we generated
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Try to connect to AWS and count how many times we retried.
+  int retries = 0;
+  Serial.print("Connecting to AWS IOT");
+
+  while (!client.connect(DEVICE_NAME) && retries < AWS_MAX_RECONNECT_TRIES) {
+    Serial.print(".");
+    delay(100);
+    retries++;
+  }
+
+  // Make sure that we did indeed successfully connect to the MQTT broker
+  // If not we just end the function and wait for the next loop.
+  if(!client.connected()){
+    Serial.println(" Timeout!");
+    return;
+  }
+
+  // If we land here, we have successfully connected to AWS!
+  // And we can subscribe to topics and send messages.
+  Serial.println("Connected!");
+}
+
+void sendJsonToAWS()
+{
+  StaticJsonDocument<128> jsonDoc;
+  JsonObject stateObj = jsonDoc.createNestedObject("state");
+  JsonObject reportedObj = stateObj.createNestedObject("reported");
+  
+  reportedObj["temperature"] = 23.76;
+  reportedObj["humidity"] = 78.12;
+  reportedObj["wifi_strength"] = WiFi.RSSI();
+
+  JsonObject locationObj = reportedObj.createNestedObject("location");
+  locationObj["name"] = "Garden";
+
+  Serial.println("Publishing message to AWS...");
+  //serializeJson(doc, Serial);
+  char jsonBuffer[512];
+  serializeJson(jsonDoc, jsonBuffer);
+
+  client.publish(AWS_IOT_TOPIC, jsonBuffer);
+}
--- AWSiot-esp32.ino
+++ AWSiot-esp32.ino
@@ -1,4 +1,30 @@
-void setup(){
+#include "certs.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+
+const char *WIFI_SSID = "KING";
+const char *WIFI_PASSWORD = "nnameking";
+
+#define DEVICE_NAME "my-arduino-device"
+
+#define AWS_IOT_ENDPOINT "xxxxxxxxxxx.iot.eu-west-1.amazonaws.com"
+
+#define AWS_IOT_TOPIC "$aws/things/" DEVICE_NAME "/shadow/update"
+
+#define AWS_MAX_RECONNECT_TRIES 50
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
+void setup() {
+  Serial.begin(9600);
+  connectToWiFi();
+  connectToAWS();
 }
-void loop(){
+void loop() {
+  sendJsonToAWS();
+  client.loop();
+  delay(1000);
 }
